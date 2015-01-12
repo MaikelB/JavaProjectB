1b30a24a64c00f47acf47e4dd1133affcaacbf84
sainsay <happy-jens@hotmail.com>
2015-01-12 09:35:07 +0100

    --adding files.

---
+++ JavaProjectB/src/Game/Deck.java
@@ -0,0 +1,18 @@
+package Game;
+
+public class Deck {
+	public Card[] cards;
+	
+	public Deck(){
+		cards = new Card[50];
+	}
+	
+	public boolean setCard( Card c ){
+		
+		return false;
+	}
+	public boolean getCard( int i ){
+		
+		return false;
+	}
+}

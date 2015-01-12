a02aca89321ec7b3e85dbe40fc8de8c87b330af5
sainsay <happy-jens@hotmail.com>
2015-01-12 14:21:13 +0100

    -- filehandler changes

--- JavaProjectB/src/Game/FileHandler.java
+++ JavaProjectB/src/Game/FileHandler.java
@@ -1,6 +1,23 @@
 package Game;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class FileHandler {
+	private List<String[]> decks = new ArrayList<String[]>();
+	private String 
+	String[] deck1 = { "deck1", "Decks\\Deck1.data"};
 	
+	public FileHandler(){
+		decks.add(deck1);
+	}
+	public void loadDeck( String deckName ){
+		for (String[] s : decks){
+			if (s[0].equals(deckName)){
+				
+			}
+		}
+	}
 
 }

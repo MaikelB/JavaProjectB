76d0f0c52377b5a4ecb5b5248cfae54f8d4e10b2
Christiaan van den berg <Yaluzan@live.nl>
2015-01-13 11:20:51 +0100

    Added line reading

--- JavaProjectB/src/Game/FileHandler.java
+++ JavaProjectB/src/Game/FileHandler.java
@@ -2,7 +2,8 @@ package Game;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import java.io.*;
+import java.util.Scanner;
 
 public class FileHandler {
 	private List<String[]> decks = new ArrayList<String[]>();
@@ -11,9 +12,52 @@ public class FileHandler {
 	public FileHandler(){
 		decks.add(deck1);
 	}
-	public void loadDeck( String deckName ){
+	public void loadDeck( String deckName ) throws IOException{
 		for (String[] s : decks){
 			if (s[0].equals(deckName)){
+				FileReader f = new FileReader(deckName);
+				BufferedReader br = new BufferedReader(f);
+				Scanner sc = new Scanner(f);
+				sc.useDelimiter("\\s = \\s|\\s : \\s");
+				while(true){
+					if (br.readLine() == "{"){
+						br.readLine();
+						if (sc.next() =="Deck_name"){
+							String dn = sc.next();
+						}
+						else if (sc.next() == "Deck_description"){
+							String dc = sc.next();
+						}
+						else {br.readLine();}			
+					}
+					else if (br.readLine() =="	{"){
+						br.readLine();
+						if (sc.next() == "Card_name"){	
+							String cn = sc.next();
+						}
+						else if (sc.next() == "Card_description"){
+							String cd = sc.next();
+						}
+						else if(sc.next() == "Card_stats"){
+							int at = sc.nextInt();
+							int def = sc.nextInt();
+						}
+						else if(sc.next() == "Card_special"){
+							int spec = sc.nextInt();							
+						}
+						else if (sc.next() == "Card_rarity"){
+							int rar = sc.nextInt();
+						}
+						else {br.readLine();}
+					}
+					else if (br.readLine() == "}"){
+						break;
+					}
+					else {
+						br.readLine();
+					}
+				}
+				sc.close();
 				
 			}
 		}

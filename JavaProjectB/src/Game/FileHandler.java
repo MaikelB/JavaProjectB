fa3417ba0021d930691681dad966413e3239d1a0
Christiaan van den berg <Yaluzan@live.nl>
2015-01-14 11:14:16 +0100

    Filehandler ready for Enemy.

--- JavaProjectB/src/Game/FileHandler.java
+++ JavaProjectB/src/Game/FileHandler.java
@@ -88,25 +88,69 @@ public class FileHandler {
 				}
 			}
 		}
-		/*
-		 * else{ for (String[] s : decks){ if (s[0].equals(deckName)){ String
-		 * read = null; FileReader f = new FileReader(s[1]); BufferedReader br =
-		 * new BufferedReader(f); Scanner sc = new Scanner(read);
-		 * sc.useDelimiter("\\s = \\s|\\s : \\s"); while(reader){
-		 * while(searchd){ read = br.readLine(); if (sc.next() =="Deck_name"){
-		 * dn = sc.next(); } else if (sc.next() == "Deck_description"){ dc =
-		 * sc.next(); } else if (br.readLine()== "\t{"){ searchc = true; break;
-		 * } } while (searchc){ read = br.readLine(); if (sc.next() ==
-		 * "Card_name"){ cn = sc.next(); } else if (sc.next() ==
-		 * "Card_description"){ cd = sc.next(); } else if(sc.next() ==
-		 * "Card_stats"){ st[0] = sc.nextInt(); st[1] = sc.nextInt(); } else
-		 * if(sc.next() == "Card_special"){ spec = sc.nextInt(); } else if
-		 * (sc.next() == "Card_rarity"){ rar = sc.nextInt();}
-		 * 
-		 * else if (br.readLine() == "\t}"){ Card a = new Card(cn, cd, spec, st,
-		 * rar); Deck.setCard(a); } else if (br.readLine() == "}"){ Deck a = new
-		 * Deck(dn, dc); Main.setEnemyDeck(a); reader = false; sc.close();
-		 * br.close(); f.close(); break; } } } } } }
-		 */
+		else{
+			for (int i = 0; i < decks.size(); i++) {
+				String[] s = decks.get(i);
+				if (s[0].equals(deckName)) {
+
+					FileReader f = new FileReader(s[1]);
+					BufferedReader br = new BufferedReader(f);
+
+					while (reader) {
+						while (searchd) {
+							read = br.readLine();
+							Scanner sc = new Scanner(read);
+							sc.useDelimiter("\\s=\\s|\\s:\\s");
+							if (sc.hasNext()) {
+
+								String scanned = sc.next();
+								if (scanned.contains("Deck_name")) {
+									dn = sc.next();
+									System.out.println(dn);
+								} else if (scanned.contains("Deck_description")) {
+									dc = sc.next();
+									System.out.println(dc);
+								} else if (scanned.contains("{")&& !scanned.contains(";{")) {
+									searchc = true;
+									searchd = false;
+								}
+							}
+							sc.close();
+						}
+						Deck b = new Deck(dn, dc);
+						Main.setEnemyDeck(b);
+						
+						while (searchc) {
+							read = br.readLine();
+							Scanner sc = new Scanner(read);
+							sc.useDelimiter("\\s=\\s|\\s:\\s");
+							if (sc.hasNext()) {
+								String scanned = sc.next();
+								if (scanned.contains("Card_name")) {
+									cn = sc.next();
+								} else if (scanned.contains("Card_description")) {
+									cd = sc.next();
+								} else if (scanned.contains("Card_stats")) {
+									st[0] = sc.nextInt();
+									st[1] = sc.nextInt();
+								} else if (scanned.contains("Card_special")) {
+									spec = sc.nextInt();
+								} else if (scanned.contains("Card_rarity")) {
+									rar = sc.nextInt();
+								} else if (scanned.contains("}") && !scanned.contains("};")) {
+									Card a = new Card(cn, cd, spec, st, rar);
+									Main.enemyDeck.setCard(a);
+								}
+								if (scanned.contains("};")){
+									reader = false;
+									searchc = false;
+								}
+							}
+							sc.close();
+						}
+					}
+				}
+			}
+		}
 	}
 }
